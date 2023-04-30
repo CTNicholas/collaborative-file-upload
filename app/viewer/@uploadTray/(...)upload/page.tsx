a78e706b3aefc226904bbc755a4cedfe3c7db1b9
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-04-30 15:05:28 +0100

    feat: Deleting spinner

--- app/viewer/@uploadTray/(...)upload/page.tsx
+++ app/viewer/@uploadTray/(...)upload/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function UploadTray() {
   return (
-    <div className="fixed top-0 right-0 bottom-0 w-1/3 min-w-[450px] max-w-full bg-white shadow-xl">
+    <div className="fixed top-0 right-0 bottom-0 w-1/3 min-w-[450px] max-w-full bg-white shadow-xl border-l">
       <div className="max-w-[400px] mx-auto pt-8">
         <FileUpload />
       </div>
--- components/FileList/File.tsx
+++ components/FileList/File.tsx
@@ -46,10 +46,10 @@ export function File({ id }: { id: string }) {
     );
   }
 
-  const { title, description, url } = file;
+  const { title, description, url, state } = file;
 
   return (
-    <li>
+    <li className={state === "deleting" ? "opacity-70" : ""}>
       <a
         href={url}
         target="_blank"
@@ -62,24 +62,49 @@ export function File({ id }: { id: string }) {
           </a>
           <div className="text-sm text-gray-600">{description}</div>
         </div>
-        <button
-          className="text-red-500 text-sm flex-grow-0 flex-shrink-0 mb-1 opacity-60 hover:opacity-100 transition-opacity duration-100"
-          onClick={deleteFile}
-        >
-          <span className="sr-only">Delete file</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
+        {state === "ready" ? (
+          <button
+            className="text-red-500 text-sm flex-grow-0 flex-shrink-0 mb-1 opacity-60 hover:opacity-100 transition-opacity duration-100"
+            onClick={deleteFile}
           >
-            <path
-              fillRule="evenodd"
-              d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
+            <span className="sr-only">Delete file</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        ) : (
+          <div>
+            <span className="sr-only">File under deletion</span>
+            <svg
+              className="animate-spin -ml-1 h-6 w-6 text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
       </div>
     </li>
   );

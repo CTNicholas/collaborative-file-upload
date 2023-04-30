6157febbe3d0388742ed294edf72bfb970e585c1
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-04-30 14:57:08 +0100

    feat: File component styled

--- components/FileUpload/FileUpload.tsx
+++ components/FileUpload/FileUpload.tsx
@@ -128,7 +128,7 @@ export function FileUpload() {
           </label>
           <input
             className="border rounded shadow-sm px-3 py-2 w-full"
-            placeholder="my-photo.png"
+            placeholder="My photo"
             id="name"
             name="name"
             type="text"

59cc4ea789b248e328c3a4fad30a4a3cec7bf3ec
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-01 17:04:29 +0100

    feat: Edge runtime

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,9 +1,7 @@
 import * as vercelBlob from "@vercel/blob";
 import { NextResponse, NextRequest } from "next/server";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "experimental-edge";
 
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url);
